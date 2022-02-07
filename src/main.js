d572f437946e1d8dcafdc45c16e2916ba985d7ea
jooniscute <chlwnsrms5@gmail.com>
2022-02-07 14:23:16 +0000

    bootstrap connected

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App";
 import router from "./routes/index.js";
-import "../scss/main.scss";
 
 // use(): plugin 연결
 createApp(App).use(router).mount("#app");
